e259581fa295731ca2ba0d2bb530d1b49d75210b
B.O.A Addington <Franswarduvar>
2022-09-28 15:31:04 -0700

    commiting new code for modules

--- models/ProductTag.js
+++ models/ProductTag.js
@@ -5,8 +5,31 @@ const sequelize = require('../config/connection');
 class ProductTag extends Model {}
 
 ProductTag.init(
+{
+  id: {
+    type: DataTypes.INTEGER,
+    allowNull: false,
+    primaryKey: true,
+    autoIncrement: true
+  }
+  },
   {
-    // define columns
+  product_id: {
+    type: DataTypes.INTEGER,
+    reference: {
+      module: "product",
+      key: "id"
+    }
+  }
+  },
+  {
+    tagId: {
+      type: DataTypes.INTEGER,
+      reference: {
+        module: "tag",
+        key: 'id'
+      }
+    }
   },
   {
     sequelize,
--- models/index.js
+++ models/index.js
@@ -5,13 +5,23 @@ const Tag = require('./Tag');
 const ProductTag = require('./ProductTag');
 
 // Products belongsTo Category
-
+Product.belongsTo(Category, {
+  foreignKey: "catagory_name"
+})
 // Categories have many Products
-
+Category.belongsTo(Product, {
+  foreignKey: "catagory_name"
+})
 // Products belongToMany Tags (through ProductTag)
-
+Product.belongsToMany(Tag, {
+  through: ProductTag,
+  foreignKey: "product_id"
+})
 // Tags belongToMany Products (through ProductTag)
-
+Tag.belongsToMany(Product, {
+through: ProductTag,
+foreignKey: "tag_N"
+})
 module.exports = {
   Product,
   Category,
